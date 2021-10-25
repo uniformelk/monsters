abe409b4b06e923cfb23101dfc61ddc7b634ad93
Cristian Orjuela <tecnologias@devsgo.com.co>
2021-10-25 11:31:06 -0500

    Plantilla constuccion finalizada

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(window).on('load', function () {
+        setTimeout(function () {
+      $("#preloader-contenedor").css({visibility:"hidden",opacity:"0", transition:" all 1s ease-out"})
+    }, 2000);
+  });
+})
